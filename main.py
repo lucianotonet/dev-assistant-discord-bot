e6f96eb1d6a80de45a0c7496efded1ce8fe4c321
Luciano T <tonetlds@gmail.com>
2023-07-26 02:58:44 -0300

    add default intents

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-﻿import os
+import os
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
 
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
 
-# Use os.getenv to get environment variable
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
