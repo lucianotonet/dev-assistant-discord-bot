726cf7a77e3702cdea80bf8cfdc08b6976c92ed9
Luciano T <tonetlds@gmail.com>
2023-07-26 02:43:05 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+﻿import os
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+# Use os.getenv to get environment variable
+bot.run(os.getenv('DISCORD_BOT_TOKEN'))
