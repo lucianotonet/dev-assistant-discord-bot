b33413918f4de7e361345b5ce98089aa093259d8
Dev Assistant AI <devassistant@tonet.dev>
2023-07-26 19:24:39 -0300

    Restore main.py functionalities

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if isinstance(message.channel, discord.DMChannel) or bot.user.mentioned_in(message):
+        await message.channel.send(f'Olá {message.author.name}, sou o Dev Assistant Discord Bot. Estou em desenvolvimento e em breve terei funcionalidades incríveis!')
+
+    await bot.process_commands(message)
+
+@bot.command(name='help_dev', help='Provides information about the bot')
+async def help(ctx):
+    help_embed = discord.Embed(title='Dev Assistant Discord Bot', description='Estou aqui para ajudar você com suas tarefas de desenvolvimento. Aqui estão alguns comandos que você pode usar:', color=0x5865F2)
+    help_embed.add_field(name='!help_dev', value='Mostra esta mensagem', inline=False)
+    await ctx.send(embed=help_embed)
 
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
